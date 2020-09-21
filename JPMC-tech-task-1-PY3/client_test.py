ca782fd9ecc99fd56b415c1cbb5a297e53d9426b
Maithri CH <maithrich3@gmail.com>
2020-09-20 22:54:46 -0700

    JPMC tech task files

---
+++ JPMC-tech-task-1-PY3/client_test.py
@@ -0,0 +1,42 @@
+import unittest
+from client3 import getDataPoint,getRatio
+
+class ClientTest(unittest.TestCase):
+  def test_getDataPoint_calculatePrice(self):
+    quotes = [
+      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote),(quote['stock'],quote['top_bid']['price'],quote['top_ask']['price'],(quote['top_bid']['price']+quote['top_ask']['price'])/2))
+
+  def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
+    quotes2 = [
+      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    """ ------------ Add the assertion below ------------ """
+    for quote in quotes2:
+      self.assertEqual(getDataPoint(quote),(quote['stock'],quote['top_bid']['price'],quote['top_ask']['price'],(quote['top_bid']['price']+quote['top_ask']['price'])/2))
+
+
+
+  """ ------------ Add more unit tests ------------ """
+def test_getRatio_calculateRatio(self):
+  price_a=100
+  price_b=10
+  self.assertEqual(getRatio(price_a,price_b),10) 
+
+def test_getRatio_calculateRatioPricebIsZero(self):
+  price_a=100
+  price_b=0
+  self.assertEqual(getRatio(price_a,price_b),0)
+
+def test_getRatio_calculateRatioPriceaIsZero(self):
+  price_a=0
+  price_b=100
+  self.assertEqual(getRatio(price_a,price_b),0)
+
+if __name__ == '__main__':
+    unittest.main()
